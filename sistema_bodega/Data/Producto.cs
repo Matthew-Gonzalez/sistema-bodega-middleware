04831a6ebe9e370d584638a2660737574e3b0781
Matthew <matthewgonzalezmansilla@gmail.com>
2021-06-19 02:22:50 -0400

    Se crea la entidad Producto

---
+++ sistema_bodega/Data/Producto.cs
@@ -0,0 +1,9 @@
+namespace sistema_bodega.Data
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Umbral { get; set; }
+    }
+}
