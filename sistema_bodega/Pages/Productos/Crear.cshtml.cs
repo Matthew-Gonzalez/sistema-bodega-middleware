ee66ee984c54a7c47fff500c9a36de7d25ab2506
Matthew <matthewgonzalezmansilla@gmail.com>
2021-06-19 02:40:55 -0400

    Modelo Crear para la entidad Producto

---
+++ sistema_bodega/Pages/Productos/Crear.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using sistema_bodega.Data;
+
+namespace sistema_bodega.Pages.Productos
+{
+    /// <summary>
+    /// Permite crear un producto en la base de datos
+    /// </summary>
+    public class CrearModel : PageModel
+    {
+        /// <summary>
+        /// Procesa la solicitud de crear un producto en la base de datos
+        /// </summary>
+        public void OnPost(string nombre, int umbral)
+        {
+            BaseDatos baseDatos = new BaseDatos();
+            Producto producto = new Producto();
+
+            producto.Nombre = nombre;
+            producto.Umbral = umbral;
+
+            baseDatos.Productos.Add(producto);
+            baseDatos.SaveChanges();
+        }
+    }
+}
