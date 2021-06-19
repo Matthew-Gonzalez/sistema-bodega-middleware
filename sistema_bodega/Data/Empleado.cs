44f1ec97f73e3e020626a7bbadf304fcb09a24de
Matthew <matthewgonzalezmansilla@gmail.com>
2021-06-19 02:19:17 -0400

    Se crea la entidad Empleado

---
+++ sistema_bodega/Data/Empleado.cs
@@ -0,0 +1,9 @@
+namespace sistema_bodega.Data
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+    }
+}
