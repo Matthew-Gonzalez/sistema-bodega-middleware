a3ce79248df66679e63fb8a2afeac4e089b40d1a
Matthew <matthewgonzalezmansilla@gmail.com>
2021-06-19 22:16:10 -0400

    Primera migracion de ProductoBodegaEmpleado

--- sistema_bodega/Migrations/20210620021302_productobodegaempleado_primera.Designer.cs
+++ sistema_bodega/Migrations/20210620021302_productobodegaempleado_primera.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sistema_bodega.Data;
 
 namespace sistema_bodega.Migrations
 {
     [DbContext(typeof(BaseDatos))]
-    partial class BaseDatosModelSnapshot : ModelSnapshot
+    [Migration("20210620021302_productobodegaempleado_primera")]
+    partial class productobodegaempleado_primera
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
