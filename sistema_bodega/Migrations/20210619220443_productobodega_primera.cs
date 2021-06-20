c91b74f3b3e5a5f793a3ffc9860cb59aebe70fbc
Matthew <matthewgonzalezmansilla@gmail.com>
2021-06-19 20:48:17 -0400

    Primera migracion de ProductoBodega

---
+++ sistema_bodega/Migrations/20210619220443_productobodega_primera.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sistema_bodega.Migrations
+{
+    public partial class productobodega_primera : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductosBodegas",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    BodegaId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductosBodegas", x => new { x.ProductoId, x.BodegaId });
+                    table.ForeignKey(
+                        name: "FK_ProductosBodegas_Bodegas_BodegaId",
+                        column: x => x.BodegaId,
+                        principalTable: "Bodegas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductosBodegas_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductosBodegas_BodegaId",
+                table: "ProductosBodegas",
+                column: "BodegaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductosBodegas");
+        }
+    }
+}
